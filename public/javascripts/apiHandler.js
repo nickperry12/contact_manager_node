a37fd25bd0a1cd150453d4fd5d914b3544de7eff
Nick <106630298+nickperry12@users.noreply.github.com>
2024-09-06 14:25:46 -0700

    Update apiHandler.js

--- public/javascripts/apiHandler.js
+++ public/javascripts/apiHandler.js
@@ -4,12 +4,12 @@ export class APIHandler {
   }
 
   async getContacts() {
-    let response = await fetch('http://localhost:3000/api/contacts');
+    let response = await fetch('http://137.184.46.7:3000/api/contacts');
     return await response.json();
   }
 
   async getContactByID(id) {
-    let response = await fetch(`http://localhost:3000/api/contacts${id}`);
+    let response = await fetch(`http://137.184.46.7:3000//api/contacts${id}`);
     return await response.json();
   }
 
@@ -23,7 +23,7 @@ export class APIHandler {
       body: JSON.stringify(data)
     }
 
-    await fetch(`http://localhost:3000/api/contacts/${id}`, options)
+    await fetch(`http://137.184.46.7:3000//api/contacts/${id}`, options)
       .then(response => {
         if (response.ok) {
           alert('Contact has been successfuly deleted.');
